d4d3be52ed628ad5e5687f0b1e14472d2af04224
leosunri <49821964+leosunri@users.noreply.github.com>
2019-04-20 21:57:16 -0300

    Create inclui.php

---
+++ inclui.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf8>
+    <title>Inclusão de novos usuários</title>
+</head>
+<body>
+<form action=salvar.php>
+<p>Nome</p>
+<input type=text name=nome>
+<p>Data</p>
+<input type=date name=data>
+<p>Salario</p>
+<input type=number name=salario>
+<br><br>
+<input type=submit value=Salvar>
+</form> 
+<br><br>
+<a href=index.php>Voltar</a>   
+</body>
+</html>
